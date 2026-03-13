616bb4dad178a161b6b2058f11b30ce3d395084c
masato3939 <masato3939>
2026-03-14 08:29:58 +0900

    change: change color

--- app/components/Access.tsx
+++ app/components/Access.tsx
@@ -18,7 +18,7 @@ const Access = () => {
         <div className="flex-1 rounded-lg overflow-hidden shadow-md">
           <iframe 
             src="https://calendar.google.com/calendar/embed?src=8130aad60c835c7476f382b04da4a60213483433df75d93413a3c2a7643e4c29%40group.calendar.google.com&ctz=Asia%2FTokyo" 
-            className="w-full h-full border-0"
+            className="w-full h-full border-0 color=%23f4511e"
           ></iframe>
         </div>
       </div>
